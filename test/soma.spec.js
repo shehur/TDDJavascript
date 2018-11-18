03fe6c124e5d3cb747f4ebb8efbd2602a10eda84
shehur <shehur5727@gmail.com>
2018-11-18 14:15:47 -0200

    implementação da função somar com parâmetros

--- test/soma.spec.js
+++ test/soma.spec.js
@@ -1,6 +1,8 @@
 const { expect } = require('code');
 const { it } = exports.lab = require('lab').script();
 
+const { somar } = require('../soma');
+
 it('a soma de 2 + 3 deve ser 5', () => {
 	expect(somar(2, 3)).to.equal(5);
 });
