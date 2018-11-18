4d1474915367776a51437c0f438d05090aabf92b
shehur <shehur5727@gmail.com>
2018-11-18 14:21:31 -0200

    implementação da função soma sem parâmetros

--- test/soma.spec.js
+++ test/soma.spec.js
@@ -6,3 +6,7 @@ const { somar } = require('../soma');
 it('a soma de 2 + 3 deve ser 5', () => {
 	expect(somar(2, 3)).to.equal(5);
 });
+
+it('a soma de 0 + 0 deve ser 0', () => {
+	expect(somar()).to.equal(0);
+});
